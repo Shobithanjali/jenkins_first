b8a57b5e92d16af79be6428f87963f70db51c61f
Shobithanjali <shobithanjaliravichandran@gmail.com>
2019-08-14 05:31:27 -0400

    jenkins

---
+++ src/main/java/controller/HomeController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+    @GetMapping
+    public String show_home(){
+            return "home";
+        }
+
+}
